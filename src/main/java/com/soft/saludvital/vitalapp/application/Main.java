ad9b0e5939b78c12da489dc37e7319eec1c87ceb
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-04-05 16:14:55 -0500

    Update Main.java

--- src/main/java/com/soft/saludvital/vitalapp/application/Main.java
+++ src/main/java/com/soft/saludvital/vitalapp/application/Main.java
@@ -4,6 +4,6 @@ package com.soft.saludvital.vitalapp.application;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola estoy ejecutando la pipeline");
+        System.out.println("Prueba de ejecucion en otro ambiente");
     }
 }
