b591bc0e5c6f14c757127f3877aa20a5dce2e3f2
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2025-04-05 15:48:53 -0500

    Update Main.java

--- src/main/java/com/soft/saludvital/vitalapp/application/Main.java
+++ src/main/java/com/soft/saludvital/vitalapp/application/Main.java
@@ -4,6 +4,6 @@ package com.soft.saludvital.vitalapp.application;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola estoy ejecutando");
+        System.out.println("Hola estoy ejecutando la pipeline");
     }
 }
