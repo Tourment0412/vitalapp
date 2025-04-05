a31123a2320fad802b7f49e02787d5db2e8fdccf
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2025-04-05 15:03:43 -0500

    Add Test implementation and build

---
+++ src/test/java/com/soft/saludvital/vitalapp/DemoTest.java
@@ -0,0 +1,12 @@
+package com.soft.saludvital.vitalapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
