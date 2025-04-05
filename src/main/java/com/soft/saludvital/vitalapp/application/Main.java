7b42c5463c228f42ed10fe3017b71f08e0b71e0f
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2025-04-05 12:11:17 -0500

    Update Main.java

--- src/main/java/com/soft/saludvital/vitalapp/application/Main.java
+++ src/main/java/com/soft/saludvital/vitalapp/application/Main.java
@@ -1,8 +1,9 @@
 package com.soft.saludvital.vitalapp.application;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hola estoy ejecutando");
     }
 }
