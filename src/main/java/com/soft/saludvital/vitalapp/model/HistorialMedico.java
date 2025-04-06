e1aa94a420aae5dc08ab3bf6beccf15c9e89ffa5
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-04-06 13:00:28 -0500

    Creacion de historial valido con base en las citas

--- src/main/java/com/soft/saludvital/vitalapp/model/HistorialMedico.java
+++ src/main/java/com/soft/saludvital/vitalapp/model/HistorialMedico.java
@@ -1,4 +1,24 @@
 package com.soft.saludvital.vitalapp.model;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class HistorialMedico {
+    @EqualsAndHashCode.Include
+    private String id;
+    private Paciente paciente;
+    private List<Cita> listaCitas;
+
+    @Builder
+    public HistorialMedico(Paciente paciente,  List<Cita> listaCitas) {
+        this.paciente = paciente;
+        this.listaCitas = listaCitas;
+    }
 }
